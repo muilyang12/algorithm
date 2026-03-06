6e786cb830e2a485cf9781dc4957e87767f011d9
Muil Yang <muilyang12@gmail.com>
2026-03-05 20:40:58 -0800

    LeetCode 55, 221 Python.

--- LeetCode - Top Interview 150 - 2/221.py
+++ LeetCode - Top Interview 150 - 2/221.py
@@ -1,5 +1,76 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        copied_matrix = [
+            [int(matrix[i][j]) for j in range(len(matrix[0]))]
+            for i in range(len(matrix))
+        ]
+
+        result = 0
+
+        for i in range(len(copied_matrix)):
+            for j in range(len(copied_matrix[0])):
+                if copied_matrix[i][j] == 0:
+                    continue
+
+                if (
+                    i > 0
+                    and j > 0
+                    and copied_matrix[i - 1][j] > 0
+                    and copied_matrix[i - 1][j - 1] > 0
+                    and copied_matrix[i][j - 1] > 0
+                ):
+                    copied_matrix[i][j] = (
+                        min(
+                            copied_matrix[i - 1][j],
+                            copied_matrix[i - 1][j - 1],
+                            copied_matrix[i][j - 1],
+                        )
+                        + 1
+                    )
+
+                result = max(result, copied_matrix[i][j])
+
+        return result**2
+
+
+"""
+["1","0","1","0","0"]
+["1","0","1","1","1"]
+["1","1","1","1","1"]
+["1","0","0","1","0"]
+
+result = 2
+
+["1","0","1","0","0"]
+["1","0","1","1","1"]
+["1","1","1","2","2"]
+["1","0","0","1","0"]
+
+Edge cases
+[["1"]]
+[["0"]]
+[
+    ["1","0"],
+    ["0","0"]
+]
+[["0","1"]]
+"""
+
+"""
+The key idea here is to build the DP memo based on the bottom-right corner of the square.
+"""
+
+"""
+Initially, I implemented a method using three nested for-loops (size, i, j). However, after discussing it with Gemini, I found out if the top-left neighbor of a cell is already part of a square, I can probably build on that. My brute force implementation was O(M * N * min(M, N)), but I think that DP approach is O(M * N)."
+"""
+
+"""
+I didn't use this for this specific problem, but I'm leaving a note here as a reminder. In Python, you can represent infinity using `math.inf`. Keep this in mind!
+"""
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
         self.matrix = matrix
         self.numRows = len(matrix)
         self.numCols = len(matrix[0])
@@ -51,8 +122,4 @@ result = 1
 Edge
 0x0
 1x1 - "1", "0"
-"""
-
-"""
-Initially, I implemented a method using three nested for-loops (size, i, j). However, after discussing it with Gemini, I found out if the top-left neighbor of a cell is already part of a square, I can probably build on that. My brute force implementation was O(M * N * min(M, N)), but I think that DP approach is O(M * N)."
 """
--- LeetCode - Top Interview 150 - 2/55.py
+++ LeetCode - Top Interview 150 - 2/55.py
@@ -1,5 +1,23 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        max_reachable = 0
+
+        for index, num in enumerate(nums):
+            if index > max_reachable:
+                return False
+
+            max_reachable = max(max_reachable, index + num)
+
+        return True
+
+
+"""
+The more I solve LeetCode questions, the more I get a feel for how to solve certain types like Binary Search, DFS, and Graph. But DP is still quite tough, and Greedy is just insanely hard.
+"""
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
         memo = [False for _ in range(len(nums))]
         memo[0] = True
 
