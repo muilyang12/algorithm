9ae8559845bf19b7ffebaea141a535c67a018738
Muil Yang <muilyang12@gmail.com>
2026-03-13 22:49:32 -0700

    LeetCode 3 Python.

--- LeetCode - Top Interview 150 - 2/3.py
+++ LeetCode - Top Interview 150 - 2/3.py
@@ -35,3 +35,9 @@ in set
 set.remove(s[left])
 left += 1
 """
+
+"""
+Looking at this problem again, I realized I solved it using a very efficient method a few days ago. I tried to rethink the solution just by reading the problem without looking at my previous code.
+This time, I momentarily thought about resetting right to left after incrementing left (`left += 1` and then `right = left`) whenever a duplicate is found. I thought like I had to restart'the scan
+from the new left position. However, there's no need to do that. I just need to keep incrementing left until the duplicate is gone. There is absolutely no reason to reset the window to zero.
+"""
