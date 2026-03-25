dc4880bad0be7e617463753941b181a81a6594f4
Muil Yang <muilyang12@gmail.com>
2026-03-24 21:44:29 -0700

    LeetCode 105, 1448 Python.

--- LeetCode - Top Interview 150 - 2/105.py
+++ LeetCode - Top Interview 150 - 2/105.py
@@ -1,3 +1,40 @@
+# Time Complexity: O(n)
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        in_node_index_map = { inorder[i]: i for i in range(len(inorder))}
+
+        def dfs_build(pre_start, pre_end, in_start, in_end):
+            in_root_index = in_node_index_map[preorder[pre_start]]
+            
+            node = TreeNode(preorder[pre_start])
+
+            node.left = dfs_build(pre_start + 1)
+            node.right = dfs_build()
+
+            return node
+
+        return dfs_build(0, len(preorder) - 1, 0, len(inorder) - 1)
+
+
+"""
+I'm not going to finish it right now. I just want to write up to that point.
+
+At first, I wrote it in a way that recursively calls the buildTree function itself using slicing. The "Solution" below follows that approach. With that method, the time complexity is O(n * (n + n)),
+which results in  O(n^2). We can optimize this in two aspects. By removing each of the n + n parts, we can eventually achieve O(n).
+
+1. I'm going to use a method that passes indices, like `def build_tree_with_index(pre_start, pre_end, in_start, in_end):`. If I do it this way, I don't need to perform slicing, so I can eliminate
+one of the n terms.
+2. For the inorder traversal, I'm planning to create a hash like { value: index }. This way, I can eliminate the use of a for-loop to find the root's index in the inorder list. This removes the
+remaining n term.
+
+In short, the time complexity will become O(n).
+
+However, thinking about it again, the second optimization requires the values to be unique. Do you remember, perhaps in the 133. Clone Graph problem, where the values weren't unique, so we had to use
+the node itself as the key? It's similar to that.
+"""
+
+
+# Time Complexity: O(n^2)
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if len(preorder) == 0 and len(inorder) == 0:
@@ -19,3 +56,11 @@ class Solution:
         )
 
         return root
+
+
+"""
+preorder = [3,9,20,15,7]
+
+inorder = [9,3,15,20,7]
+             r
+"""
--- LeetCode - Top Interview 150 - 2/1448.py
+++ LeetCode - Top Interview 150 - 2/1448.py
@@ -1,5 +1,42 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        def dfs_search(node, max_so_far):
+            if not node:
+                return 0
+
+            count = 0
+
+            count += dfs_search(node.left, max(max_so_far, node.val))
+            count += dfs_search(node.right, max(max_so_far, node.val))
+
+            if node.val >= max_so_far:
+                count += 1
+            
+            return count
+
+        return dfs_search(root, -math.inf)
+
+
+"""
+I like the approach of finding Good Nodes using `max_so_far`. However, using the nonlocal keyword to directly modify a value outside the `dfs` function felt a bit off to me. I had a brief chat
+with Gemini about this, and it suggested an alternative. It said like how about having the function return the current count instead?
+
+There are two key takeaways from this problem
+1. While passing the `path` works, passing `max_so_far` is even better. This idea is similar to the approach used in problems like "Best Time to Buy and Sell Stock" or "Trapping Rain Water."
+2. Let's aim to design `dfs` functions so that they return something like a Node or a value. It feels like this opens up many more possibilities.
+"""
+
+
+"""
+But I didn't handle this problem very well. It's because when I first called the `dfs_search` function, I carelessly set the `max_so_far` value to 0. I shouldn't just make assumptions in my head.
+I should always ask questions and communicate. I should have asked a question during that process especially about the minimum possible value that could be in a node. If there's an assumption
+that values are positive, starting at -1 or 0 would be fine. But that wasn't the case, right? In that situation, I should have started from negative infinity. You know that proceeding without
+checking these details is a really bad signal, right? I need to be careful.
+"""
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
         count = 0
 
         def dfs_search(node, max_so_far):
@@ -19,6 +56,17 @@ class Solution:
         return count
 
 
+"""
+Since I've used the method of storing and accumulating paths a few times for this type of problem, I initially used the approach shown below, passing the `path` and recording everything visited
+until reaching the current node. Of course, if I use a path like this, I'll have to push and pop, right? In this case, the Space Complexity would be O(log n) for a balanced tree, but in the worst
+case, it could grow up to O(n). So, it's not very efficient.
+
+Gemini suggested that I try passing only the `max_value`. Especially since a node is a "Good Node" if its current value is greater than the maximum of all values encountered so far. So, the idea
+is to just keep passing down the maximum value from the root to the current point. Haven't we seen this type of problem quite often?
+"""
+
+
+# Time Complexity: O(nh), O(n^2) at worst, Space Complexity: O(h), O(log n), O(n) at worst
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         count = 0
