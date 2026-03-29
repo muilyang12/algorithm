8a1e7006a42482ac3ed5e0a414f57a09ce3ff5d3
Muil Yang <muilyang12@gmail.com>
2026-03-29 16:07:18 -0700

    LeetCode 104, 572 Python.

--- LeetCode - Top Interview 150 - 2/104.py
+++ LeetCode - Top Interview 150 - 2/104.py
@@ -1,5 +1,27 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs_get_height(node):
+            if not node:
+                return 0
+
+            return max(dfs_get_height(node.left), dfs_get_height(node.right)) + 1
+
+        return dfs_get_height(root)
+
+
+"""
+When trying to solve this problem using DFS, one approach is to pass the `depth` as a parameter and update a `nonlocal depth` variable. However, I personally dislike using `nonlocal`
+for these kinds of solutions.
+
+Of course, there is also the BFS approach, which can solve this problem quite easily as shown in the implementation below.
+
+But since we have been practicing tree traversal in a way where the DFS function returns a value, it seems better to write it that way. In this case, the DFS function would recursively
+calculate the heights of the left and right subtrees, add one to the maximum of the two, and then return that value.
+"""
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
 
--- LeetCode - Top Interview 150 - 2/572.py
+++ LeetCode - Top Interview 150 - 2/572.py
@@ -1,3 +1,57 @@
+# Time Complexity: O(n + m) or O(nm), Space Complexity: O(n + m)
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def dfs_is_same_tree(original_node, node_to_compare):
+            if not original_node and not node_to_compare:
+                return True
+            elif not original_node or not node_to_compare:
+                return False
+            elif original_node.val != node_to_compare.val:
+                return False
+            else:
+                return dfs_is_same_tree(
+                    original_node.left, node_to_compare.left
+                ) and dfs_is_same_tree(original_node.right, node_to_compare.right)
+
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            node = queue.popleft()
+            if node.val == subRoot.val and dfs_is_same_tree(node, subRoot):
+                return True
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return False
+
+
+"""
+Same number can exist?
+"""
+
+"""
+이렇게 사용을 하면 하나만 None인 경우까지 짧은 if 문으로 커버가 가능하다.
+
+`if not original_node and not node_to_compare:`
+
+`elif not original_node or not node_to_compare:`
+"""
+
+"""
+Calculating the complexity for this problem is quite tricky.
+
+1. For the Time Complexity, the ideal scenario would be encountering a node in the root tree that matches the value of the subRoot and finding that they are identical. 
+In such a case, the complexity would be O(n + m). However, the worst-case scenario occurs when nodes with the same value appear repeatedly, and the overall tree
+structure remains very similar. In that situation, the time complexity could degrade to O(nm), which is not very efficient.
+2. As for the Space Complexity, we must account for both the space occupied by the queue during traversal and the space used by the DFS recursion stack. Therefore, it
+would be O(n + m).
+"""
+
+
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         def is_same_tree(head1, head2):
