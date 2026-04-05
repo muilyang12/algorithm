42e37b1b839760583861facc0b244261a8e5ea09
Muil Yang <muilyang12@gmail.com>
2026-04-05 07:40:44 -0700

    LeetCode 139, 322 Python.

--- LeetCode - Top Interview 150 - 2/139.py
+++ LeetCode - Top Interview 150 - 2/139.py
@@ -1,3 +1,28 @@
+"""
+There are a few key elements to remember in Dynamic Programming.
+1. You must derive the current `memo` value by referencing previous `memo` values. A method where you determine future `memo` values using the current value is not permitted.
+2. When setting memo[i], you often need to use functions such as min or max, so you must be cautious when determining the initial values of the memo array.
+"""
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = [False for _ in range(len(s) + 1)]
+        memo[0] = True
+
+        for i in range(1, len(memo)):
+            for word in wordDict:
+                if i - len(word) < 0:
+                    continue
+
+                if not memo[i - len(word)] or s[i - len(word) : i] != word:
+                    continue
+
+                memo[i] = True
+
+        return memo[-1]
+
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False for _ in range(len(s) + 1)]
--- LeetCode - Top Interview 150 - 2/322.py
+++ LeetCode - Top Interview 150 - 2/322.py
@@ -1,3 +1,30 @@
+"""
+This problem is identical to "279. Perfect Squares".
+
+There are two key elements to remember when solving DP problems.
+1. The `memo` array should always be filled by referencing previous values to determine the current one. You should not use a method like `memo[i + coin] = memo[i] + ...` to update
+future values.
+2. It is crucial to set the initial values of the memo array correctly. Since the goal here is to find the minimum value, initializing the memo with math.inf is the best approach.
+
+I used to initialize `memo` array with 0 out of habit. I must keep the above precautions in mind.
+"""
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = [math.inf for _ in range(amount + 1)]
+        memo[0] = 0
+
+        for i in range(1, len(memo)):
+            for coin in coins:
+                if i - coin < 0:
+                    continue
+
+                memo[i] = min(memo[i], memo[i - coin] + 1)
+
+        return memo[amount] if memo[amount] != math.inf else -1
+
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = [math.inf for _ in range(amount + 1)]
