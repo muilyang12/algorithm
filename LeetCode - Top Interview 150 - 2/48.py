407ab04e5f766fc5c64027c4fcc8e30c070902ae
Muil Yang <muilyang12@gmail.com>
2026-04-07 07:12:48 -0700

    LeetCode 48, 50 Python.

---
+++ LeetCode - Top Interview 150 - 2/48.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # for
+        #     for
+        #         num1 = loc1
+        #         num2 = loc2
+        #         num3 = loc3
+        #         num4 = loc4
+        
+        #         loc1 = num4
+        #         loc2 = num1
+        #         loc3 = num2
+        #         loc4 = num3
--- LeetCode - Top Interview 150 - 2/50.py
+++ LeetCode - Top Interview 150 - 2/50.py
@@ -1,3 +1,24 @@
+"""
+x = 2.00000, n = 10
+
+memo = [2, 4, 8, ...]
+
+=====
+
+(2, 10)
+
+(2, 5), (2, 5)
+
+(2, 2), (2, 3), (2, 2), (2, 3)
+
+memo = {2: 4, 3: 8, 5: 32}
+
+=====
+
+Time Complexity: O(n), Space Complexity: O(log n + log n) = O(log n)
+"""
+
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         self.memo = {0: 1}
