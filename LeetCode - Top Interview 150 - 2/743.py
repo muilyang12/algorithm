fc5bc7f3f6a632ca7e341fba40c6afa79ca2a0cc
Muil Yang <muilyang12@gmail.com>
2026-04-07 09:16:55 -0700

    LeetCode 743 Python.

---
+++ LeetCode - Top Interview 150 - 2/743.py
@@ -0,0 +1,15 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # graph = { from_node: [(to_nodes, time)]}
+        # times = [math.inf for count]
+        # times[from] = 0
+
+        # heap = [(0, start)]
+        # while heap:
+        # target, dis
+        #  if times[target] < dis:
+        #     continue
+        #     for times[target]
+        #         next
+                
+        #         heapq.heappush()
